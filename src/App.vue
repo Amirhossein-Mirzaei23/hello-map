484ed4ce017302bde9a85b2f10e758cb152e8a44
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-05 23:56:09 +0330

    create  base structure of project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MapViewVue />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MapViewVue from './components/MapView.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MapViewVue
   }
 }
 </script>
---
+++ src/components/MapView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div style="height:600px;">
+      <l-map @ready="onReady" 
+       @locationfound="onLocationFound" 
+       ref="map" 
+       v-model:zoom="zoom" 
+       v-model:center="center"
+       :min-zoom="minZoom"
+       :max-zoom="maxZoom"
+       >
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        ></l-tile-layer>
+        <l-marker
+        :lat-lng="marker"
+        
+      />
+      </l-map>
+    </div>
+  </template>
+  
+  <script setup >
+  import "leaflet/dist/leaflet.css";
+  import { ref } from 'vue'
+  import { LMap, LTileLayer ,LMarker } from "@vue-leaflet/vue-leaflet";
+
+  let zoom  = 15
+  let minZoom = 5
+  let maxZoom = 18
+let center = ref([36.299, 59.601])
+
+
+let marker = ref([36.361, 59.508]);
+
+function onReady (mapObject) {
+  mapObject.locate();
+  console.log(mapObject.locate());
+}
+function onLocationFound(location){
+  marker.value = location
+  center.value = location
+}
+
+  </script>
+  
+  <style></style>
