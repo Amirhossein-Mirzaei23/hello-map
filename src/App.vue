5a4c3296bc83b21e41dd936b0829f17d0d625ada
eren <mjsh1385@gmail.com>
2024-06-06 11:06:49 +0330

    add tailwind and geolocation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="bg-sky-300 bg-opacity-75" >
+   
     <MapViewVue />
   </div>
 </template>
@@ -23,6 +24,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
